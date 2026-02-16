b8e7afd51ca3287055f656383cfe6fb3380f4cad
Yesuraj <joseph.yesuraj@capgemini.com>
2026-02-17 01:03:52 +0530

    Variables

---
+++ variables.py
@@ -0,0 +1,14 @@
+My_Var= "HI"
+print(My_Var)
+
+# True = 123
+# print(True)   # Shows an Error / Keyword should not be used 
+
+list = "jose"
+print(type(list)) 
+
+_var1 = "Hello python"
+print(_var1)
+
+12 = "Welcome"
+print(12)          # Shows an Error / Case sensitive - Variables should be start with alphabetic and underscore
